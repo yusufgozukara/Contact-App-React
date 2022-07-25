40b43847e5ee62e3b8f0e4e838f04b774acd31f5
yusufgozukara <yusufgozukara11@gmail.com>
2022-07-25 16:09:35 +0300

    states added

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,27 +1,51 @@
-import React from 'react'
+import { useState } from "react";
 
 const Form = () => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [gender, setGender] = useState('');
+  console.log(name, phone, gender);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setName('');
+    setPhone('');
+    setGender('');
+    
+  }
+
   return (
-    <div className='formContainer'>
-        <div className='formDiv'>
-            <h1>Add Contact</h1>
-            <form>
-                <input type="text"  placeholder='Name'  />
-                <input type="text" name="" id=""placeholder='Phone number'/>
-                <select id="cars" name="cars">
-                    <option value="gender" disabled>Gender</option>
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                    <option value="other">Other</option>
-                </select>
-                <button type='submit'>Add</button>
-                
-            </form>
-
-        </div>
-
+    <div className="formContainer">
+      <div className="formDiv">
+        <h1>Add Contact</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Phone number"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+          <select id="cars" name="cars" onChange={(e) => setGender(e.target.value)}>
+            <option value="gender" selected disabled>
+              Gender
+            </option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+          <button type="submit">Add</button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
