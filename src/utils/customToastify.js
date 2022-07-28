9ea7bba0a1fe08d3b2c1b77a6c5dfbd52fb828e0
yusufgozukara <yusufgozukara11@gmail.com>
2022-07-28 08:47:27 +0300

    toastify time updated

--- src/utils/customToastify.js
+++ src/utils/customToastify.js
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 export const toastSuccessNotify = (msg) => {
     toast.success(msg, {
         position: "top-center",
-      autoClose: 3000,
+      autoClose: 2000,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
